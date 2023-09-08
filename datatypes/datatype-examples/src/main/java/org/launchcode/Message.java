959df444c38195f090c7c0efdc712ee09eff692e
lorikovarik80 <kovarik.lori@gmail.com>
2023-09-07 20:27:29 -0500

    studio 5.2 complete

--- datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("rus")) {
+            return "Привет, мир";
         } else {
             return "Hello, World!";
         }
