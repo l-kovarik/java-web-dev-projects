90be16a2b56ed79c74b887458e0ffb70d9948837
lorikovarik80 <kovarik.lori@gmail.com>
2023-09-11 16:09:52 -0500

    Unit testing exercises complete

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Solution.java
@@ -0,0 +1,63 @@
+//package org.launchcode;
+//
+//import javax.swing.*;
+//import java.util.ArrayList;
+//import java.util.Date;
+//
+//public class Solution {
+//
+////    MenuItem
+//    private double price;
+//    private String description;
+//    private String category;
+//    private boolean isNew;
+//
+//    public MenuItem(double p, String d, String c, boolean iN) {
+//        this.price = p;
+//        this.description = d;
+//        this.category = c;
+//        this.isNew = iN;
+//    }
+//
+//    public void setPrice(double price) {
+//        this.price = price;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public void setCategory(String category) {
+//        this.category = category;
+//    }
+//
+//    public void setNew(boolean aNew) {
+//        isNew = aNew;
+//    }
+//
+////    menu
+//private Date lastUpdated;
+//    private ArrayList<MenuItem> items;
+//
+//    public Menu(Date d, ArrayList<MenuItem> i) {
+//        this.lastUpdated = d;
+//        this.items = i;
+//    }
+//
+//    public void setLastUpdated(Date lastUpdated) {
+//        this.lastUpdated = lastUpdated;
+//    }
+//
+//    public void setItems(ArrayList<MenuItem> items) {
+//        this.items = items;
+//    }
+//
+//    public Date getLastUpdated() {
+//        return lastUpdated;
+//    }
+//
+//    public ArrayList<MenuItem> getItems() {
+//        return items;
+//    }
+//
+//}
--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -14,18 +14,42 @@ class CarTest {
     }
 
     //TODO: add emptyTest so we can configure our runtime environment
-    @Test
-    public void emptyTest() {
-        assertEquals(10, 10, .001);
-    }
+//    @Test
+//    public void emptyTest() {
+//        assertEquals(10, 10, .001);
+//    }
 
     //TODO: constructor sets gasTankLevel properly
     @Test
     public void testInitialGasTank() {
-        assertEquals( 10, test_car.getGasTankLevel(),.001);
+        assertEquals(10, test_car.getGasTankLevel(), .001);
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void gasTankAccurate() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void gasTankAfterExceedsRange() {
+        test_car.drive(502);
+        assertEquals(0, test_car.getGasTankLevel(), .001);
+    }
+
+    @Test
+    public void milesAvailableAfterExceedsRange() {
+        test_car.drive(502);
+        assertEquals(500, test_car.getOdometer(), .001);
+    }
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test()
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () ->
+            test_car.addGas(5), "Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    };
+
 }
