b268622314ceffc58fdc9569a35fa4b5db642e60
lorikovarik80 <kovarik.lori@gmail.com>
2023-09-18 20:42:26 -0500

    spin studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
         // TODO: Declare and initialize a CD and a DVD object.
 
+        CD compactDisc = new CD("New CD", "compact disc", 5.5, "mp3", "500MB", 12);
+        DVD movie = new DVD ("Video", "bluRay", 5.5, "MP4", "4G");
         // TODO: Call each CD and DVD method to verify that they work as expected.
+    compactDisc.spin();
+        System.out.println(compactDisc.readyToUse);
+
+       compactDisc.readDisc();
+
     }
 }
