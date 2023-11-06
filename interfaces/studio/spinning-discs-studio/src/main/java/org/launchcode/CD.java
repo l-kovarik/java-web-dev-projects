36abee63630c9437061f5c39fa1db395f6fb7559
lorikovarik80 <kovarik.lori@gmail.com>
2023-11-06 13:10:22 -0600

    interface studio partially completed

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,33 +1,79 @@
 package org.launchcode;
 
-public class CD extends InfoDisc implements DiscInterface {
-    // TODO: Implement your custom interface.
+public class CD extends Media implements Rewritable{
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
-    int numTracks;
+    private boolean isMusicCD;
 
-    public CD(String title, String discType, double diameter, String format, String storageCapacity, int numTracks) {
-        super(title, discType, diameter, format, storageCapacity);
-        this.numTracks = numTracks;
-        this.readyToUse = false;
+    public CD(String name, boolean isMusicCD) {
+        super(name, "", 800, 700);
+        this.isMusicCD = isMusicCD;
+        setDiscType(isMusicCD ? "music CD" : "CD-RW");
+    }
+
+    public CD (String name) {
+        this(name, false);
+    }
+
+    public boolean isMusicCD() {
+        return isMusicCD;
+    }
+
+    public void setMusicCD(boolean musicCD) {
+        isMusicCD = musicCD;
     }
 
     @Override
-    public void spin() {
-        readyToSpin();
-        System.out.println(title + " is spinning between " + rpm + "RPMs.");
+    public String toString() {
+        String header = isMusicCD ? "Tracks" : "Files";
+        return super.toString() + getFormattedFileList(header);
+    }
+
+    //Methods required by rewritable
+
+
+    @Override
+    public void writeFile(File file) {
+        spinDisc();
+        String fileType = isMusicCD ? "track" : "file";
+        if (getFiles().contains(file)) {
+            System.out.println("The filetype is " + fileType + " " + file.getName() + " has already been added.");
+        } else if (getSpaceUsed() + file.getSize() > getCapacity()) {
+            System.out.println("Warning: There is not enough space on the " + getDiscType() + " for " + file.getName() + ".");
+        } else {
+            getFiles().add(file);
+            System.out.println("The " + fileType + " " + file.getName() + "has been added to " + getName() + ".");
+        }
     }
 
     @Override
-    public void readyToSpin() {
-        System.out.println(readyToUse);
+    public void removeFile(File file) {
+        if (isMusicCD) {
+            System.out.println("Individual files cannot be removed from a CD once written.");
+        } else {
+            spinDisc();
+            if (fileIsPresent(file)) {
+                getFiles().remove(file);
+                System.out.println("The file " + file.getName() + " has been removed from the " + getDiscType() + ".");
+            }
+        }
     }
 
     @Override
-    public void readDisc() {
-        String newline = System.lineSeparator();
-        System.out.println("Disc is readable "+newline+ title);
+    public void reformatDisc() {
+        spinDisc();
+        getFiles().clear();
+        setMusicCD(false);
+        setDiscType("CD-RW");
+        setName("Empty Disc");
+    }
+
+    @Override
+    public void runFile(File file) {
+        if (fileIsPresent(file)) {
+            spinDisc();
+            String verb = isMusicCD ? "Playing " : "Opening file ";
+            System.out.println(verb + file.getName() + "...");
+        }
 
     }
 }
--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -3,15 +3,5 @@ package org.launchcode;
 public class Main {
     public static void main(String[] args) {
 
-        // TODO: Declare and initialize a CD and a DVD object.
-
-        CD compactDisc = new CD("New CD", "compact disc", 5.5, "mp3", "500MB", 12);
-        DVD movie = new DVD ("Video", "bluRay", 5.5, "MP4", "4G");
-        // TODO: Call each CD and DVD method to verify that they work as expected.
-    compactDisc.spin();
-        System.out.println(compactDisc.readyToUse);
-
-       compactDisc.readDisc();
-
     }
 }
